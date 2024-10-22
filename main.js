dc98f03ed457d177917e6a93b7f8ada54cb7f843
madeleinc <madeleine.cancela@stu.ocsb.ca>
2024-10-22 13:02:16 -0400

    part1 html done!

--- main.js
+++ main.js
@@ -40,3 +40,31 @@ function vertex() {
 
 }
 
+//1. Creating a Javascript function delta(a,b) which returns the valueof a - b.
+function delta(a,b) {
+    return (a - b);
+
+}
+
+//2. Creating the function slope(x1,y1,x2,y2) which reiceives the points (x1,y1),(x2,y2) and returns the slope without rounding the result. 
+function slope(x1,y1,x2,y2) {
+    let m = delta(y2, y1)/ delta(x2, x1)
+    return m;
+
+}
+
+//3. Creating the function average(n1,n2) which returns the average of the two numbers.
+function average (n1,n2) {
+    let average = (n1 + n2)/2
+    return average;
+
+}
+
+//4. Creating the function round_user(value) which takes the value and rounds it to the user's request number of decimals, returning the answer.
+
+
+//Creating the function length(x1,y1,x2,y2) whih receives the points (x1,y1),(x2,y2) and reutrns the distance between the two points. 
+function length(x1,y1,x2,y2){
+    let distance = Math.sqrt(delta(x2,x1)**2 + delta(y2,y1)**2)
+    return distance;
+}
