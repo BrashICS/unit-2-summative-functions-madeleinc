b684e85621e34dfe916ac2ca1b9cc80395f6beee
madeleinc <madeleine.cancela@stu.ocsb.ca>
2024-10-24 13:47:19 -0400

    part2done!!!!!!!!!

--- main.js
+++ main.js
@@ -9,8 +9,8 @@
 'use strict';
 
 /*** Event Listeners ***/
-document.getElementById("zeros").addEventListener("click", zeros);
-document.getElementById("vertex").addEventListener("click", vertex);
+document.getElementById("zeros_button").addEventListener("click", zeros);
+document.getElementById("vertex_button").addEventListener("click", vertex);
 document.getElementById("rect_volume_button").addEventListener("click", rect_prism_volume);
 document.getElementById("rect_area_button").addEventListener("click", rect_prism_area);
 document.getElementById("sphere_volume_button").addEventListener("click", sphere_volume)
@@ -24,15 +24,32 @@ document.getElementById("midpoint_button").addEventListener("click", midpoint)
 
 // Calculate the y-value of a parabola from standand form
 function y_quad(a, b, c, x) {
+    let y = a*(x**2) + b*x + c
+    return y
 
 }
 
 // Determine the zeros of a quadratic using user-inputs for a, b, and c
 function zeros() {
+    let a = Number(document.getElementById("a").value);
+    let b = Number(document.getElementById("b").value);
+    let c = Number(document.getElementById("c").value);
+    let zeros_answer_1 = round_user((-1 * b) + (Math.sqrt(b**2 - (4 * a * c)))/ (2 * a));
+    let zeros_answer_2 = round_user((-1 * b) - (Math.sqrt(b**2 - (4 * a * c)))/ (2 * a));
+    document.getElementById("quadratic_zeros_output").textContent = `The quadratic's zeros are (${zeros_answer_1},0) and (${zeros_answer_2},0).`;
+    console.log(`Zeroes are (${zeros_answer_1},0) and (${zeros_answer_2},0).`)
+    
 }
 
 // Determine the vertex of a quadratic using user-inputs for a, b, and c
 function vertex() {
+    let a = Number(document.getElementById("a").value);
+    let b = Number(document.getElementById("b").value);
+    let c = Number(document.getElementById("c").value);
+    let x = round_user((-1 * b)/(2 * a))
+    let y = round_user(y_quad(a, b, c, x))
+    document.getElementById("quadratic_vertex_output").textContent = `The quadratic vertex is (${x},${y}).`;
+    console.log(`Vertex is (${x},${y})`);
 
 }
 
@@ -76,6 +93,7 @@ function rect_prism_volume(){
     let height = Number(document.getElementById("length").value);
     let rect_volume_answer = round_user(length * width * height); 
     document.getElementById("rect_volume_output").textContent = `The volume of the rectangular prism is ${rect_volume_answer}.`;
+    console.log(`Rectangular prism volume is ${rect_volume_answer}.`);
 
 }
 
@@ -86,6 +104,7 @@ function rect_prism_area(){
     let height = Number(document.getElementById("height").value);
     let rect_area_answer = round_user(2*(width*length+height*length+height*width));
     document.getElementById("rect_area_output").textContent = `The area of the rectangular prism is ${rect_area_answer}.`;
+    console.log(`Rectangular prism area is ${rect_area_answer}.`);
 }
 
 //Calculating sphere volume for wepbage
@@ -93,6 +112,7 @@ function sphere_volume(){
     let radius = Number(document.getElementById("radius").value);
     let sphere_volume_answer = round_user(4/3*Math.PI*radius**3);
     document.getElementById("sphere_volume_output").textContent = `The volume of the sphere is ${sphere_volume_answer}.`;
+    console.log(`Sphere volume is ${sphere_volume_answer}.`);
 }
 
 //Calculating sphere area for wepbage
@@ -100,6 +120,7 @@ function sphere_area(){
     let radius = Number(document.getElementById("radius").value);
     let sphere_area_answer = round_user(4*Math.PI*radius);
     document.getElementById("sphere_area_output").textContent = `The area of the sphere is ${sphere_area_answer}.`;
+    console.log(`Sphere area is ${sphere_area_answer}.`);
 }
 
 //Calculating line slope for wepbage
@@ -110,6 +131,7 @@ function line_slope(){
     let y2 = Number(document.getElementById("y2").value);
     let slope_answer = round_user(slope(x1,y1,x2,y2));
     document.getElementById("line_slope_output").textContent = `The slope of the line is ${slope_answer}.`;
+    console.log(`Slope of line is ${slope_answer}.`);
 }
 
 //Calculating line length for wepbage
@@ -120,6 +142,7 @@ function line_length(){
     let y2 = Number(document.getElementById("y2").value);
     let length_answer = round_user(length(x1,y1,x2,y2));
     document.getElementById("line_length_output").textContent = `The length of the line is ${length_answer}.`;
+    console.log(`Length of line is ${length_answer}.`);
 }
 
 //Calculating line midpoint for wepbage
@@ -131,4 +154,5 @@ function midpoint(){
     let midpoint_x = round_user(average(x1,x2));
     let midpoint_y = round_user(average(y1,y2));    
     document.getElementById("midpoint_output").textContent = `The midpoint of the line is (${midpoint_x},${midpoint_y}).`;
+    console.log(`Midpoint of line is (${midpoint_x},${midpoint_y}).`);
 }
