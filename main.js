9b1c5a4b40a17025179c346cb8beba8c01a15498
madeleinc <madeleine.cancela@stu.ocsb.ca>
2024-10-24 22:18:14 -0400

    :^(

--- main.js
+++ main.js
@@ -24,21 +24,18 @@ document.getElementById("vertex_button").addEventListener("click", vertex);
 //PART1 JS - 1. Creating the function delta(a,b) which returns the value of a - b.
 function delta(a,b) {
     return (a - b);
-
 }
 
 //PART1 JS - 2. Creating the function slope(x1,y1,x2,y2) which receives the points (x1,y1),(x2,y2) and returns the slope.
 function slope(x1,y1,x2,y2){
     let m = delta(y2, y1)/ delta(x2, x1);
     return m;
-
 }
 
 //PART1 JS - 3. Creating the function average(n1,n2) which returns the average of two numbers.
 function average (n1,n2){
     let average = (n1 + n2)/2;
     return average;
-
 }
 
 //PART1 JS - 4. Creating the function round_user(value) which takes the value and rounds it to the user's requested number of decimals.
@@ -46,14 +43,12 @@ function round_user(value){
     let decimals = Number(document.getElementById("rounding").value);
     let decimals2 = Math.round(value * 10**decimals) / 10**decimals;
     return decimals2;
-
 }
 
 //PART1 JS - 5. Creating the function length(x1,y1,x2,y2) whih receives the points (x1,y1),(x2,y2) and returns the distance between the two points. 
 function length(x1,y1,x2,y2){
     let distance = Math.sqrt(delta(x2,x1)**2 + delta(y2,y1)**2);
     return distance;
-
 }
 
 //PART1 "HTML" - 7. Calculating the rectangular prism's volume for webpage using user's input.
@@ -64,7 +59,6 @@ function rect_prism_volume(){
     let rect_volume_answer = round_user(length * width * height); 
     document.getElementById("rect_volume_output").textContent = `The volume of the rectangular prism is ${rect_volume_answer}.`;
     console.log(`Rectangular prism volume is ${rect_volume_answer}.`);
-
 }
 
 //PART1 "HTML" - 8. Calculating the rectangular prism's area for webpage using user's input.
@@ -75,7 +69,6 @@ function rect_prism_area(){
     let rect_area_answer = round_user(2*(width*length+height*length+height*width));
     document.getElementById("rect_area_output").textContent = `The area of the rectangular prism is ${rect_area_answer}.`;
     console.log(`Rectangular prism area is ${rect_area_answer}.`);
-
 }
 
 //PART1 "HTML" - 9. Calculating the sphere's volume and area for wepbage using user's input.
@@ -84,7 +77,6 @@ function sphere_volume(){
     let sphere_volume_answer = round_user(4/3*Math.PI*radius**3);
     document.getElementById("sphere_volume_output").textContent = `The volume of the sphere is ${sphere_volume_answer}.`;
     console.log(`Sphere volume is ${sphere_volume_answer}.`);
-
 }
 
 function sphere_area(){
@@ -92,7 +84,6 @@ function sphere_area(){
     let sphere_area_answer = round_user(4*Math.PI*radius);
     document.getElementById("sphere_area_output").textContent = `The area of the sphere is ${sphere_area_answer}.`;
     console.log(`Sphere area is ${sphere_area_answer}.`);
-
 }
 
 //Calculating line segment's slope for webpage user user's input. 
@@ -104,7 +95,6 @@ function line_slope(){
     let slope_answer = round_user(slope(x1,y1,x2,y2));
     document.getElementById("line_slope_output").textContent = `The slope of the line is ${slope_answer}.`;
     console.log(`Slope of line is ${slope_answer}.`);
-
 }
 
 //Calculating the line's length for wepbage using user's input.
@@ -116,7 +106,6 @@ function line_length(){
     let length_answer = round_user(length(x1,y1,x2,y2));
     document.getElementById("line_length_output").textContent = `The length of the line is ${length_answer}.`;
     console.log(`Length of line is ${length_answer}.`);
-
 }
 
 //PART1 - 12. Calculating the line's midpoint for wepbage using user's input.
@@ -129,14 +118,12 @@ function midpoint(){
     let midpoint_y = round_user(average(y1,y2));    
     document.getElementById("midpoint_output").textContent = `The midpoint of the line is (${midpoint_x},${midpoint_y}).`;
     console.log(`Midpoint of line is (${midpoint_x},${midpoint_y}).`);
-
 }
 
 //PART2 JS - 1. Creating the function y_quad(a,b,c,x) which takes the coefficients from the standard form quadratic equation.
 function y_quad(a, b, c, x) {
     let y = a*(x**2) + b*x + c
     return y
-
 }
 
 //PART2 "HTML" - 2.  Determining the zeros of a quadratic using user-inputs for a, b, and c
@@ -147,8 +134,7 @@ function zeros() {
     let zeros_answer_1 = round_user((-1 * b) + (Math.sqrt(b**2 - (4 * a * c)))/ (2 * a));
     let zeros_answer_2 = round_user((-1 * b) - (Math.sqrt(b**2 - (4 * a * c)))/ (2 * a));
     document.getElementById("quadratic_zeros_output").textContent = `The quadratic's zeros are (${zeros_answer_1},0) and (${zeros_answer_2},0).`;
-    console.log(`Zeroes are (${zeros_answer_1},0) and (${zeros_answer_2},0).`)
-    
+    console.log(`Zeroes are (${zeros_answer_1},0) and (${zeros_answer_2},0).`) 
 }
 
 //PART2 "HTML" - 3. Determining the vertex of a quadratic using user-inputs for a, b, and c
@@ -160,5 +146,4 @@ function vertex() {
     let y = round_user(y_quad(a, b, c, x))
     document.getElementById("quadratic_vertex_output").textContent = `The quadratic vertex is (${x},${y}).`;
     console.log(`Vertex is (${x},${y})`);
-
 }
